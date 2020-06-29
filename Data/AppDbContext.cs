db77d9c4cfdcfa44b0f35da201b8c91b6472ed7f
azizul arif <you@example.com>
2020-06-29 13:28:56 +0600

    v0.1

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using RestManage.Models;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace RestManage.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public Microsoft.EntityFrameworkCore.DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace RestManage.Models
+{
+    public class Employee
+    {
+        public string EmployeeName { get; set; }
+        public string Dob { get; set; }
+    }
+}
